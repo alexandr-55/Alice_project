4cc8f9ea3475c8869a931c7184dd117e8ee2aa90
Smart developer <developer@yandex.ru>
2019-04-24 21:59:33 +0300

    v 2.0

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 ﻿# импортируем библиотеки
 from flask import Flask, request
 import logging
+import random
 
 import json
 
@@ -46,10 +47,14 @@ def handle_dialog(req, res):
         sessionStorage[user_id] = {
             'suggests': [
                 "Число загадано.",
-                "Не хочу.",
+                "Не хочу играть.",
                 "Отстань!"
             ],
-            'first_name': None
+            'first_name': None,
+            'yes_no' : [
+                "Да",
+                "Нет"
+            ]
         }
         # создаём словарь, диапазона чисел
         sessiondiap[user_id] = {
@@ -81,7 +86,7 @@ def handle_dialog(req, res):
             sessionStorage[user_id]['first_name'] = first_name
             res['response'][
                 'text'] = 'Приятно познакомиться!     ' + first_name.title()\
-                          + ',   загадай число от 1 до 1000, а я попробую его отгадать.'
+                          + ',   загадай число от 1 до 100, а я попробую его отгадать.'
             res['response']['buttons'] = get_suggests(user_id)
             sessiondiap[user_id]['regim'] = 'загадай'
         return
@@ -92,23 +97,54 @@ def handle_dialog(req, res):
             sessiondiap[user_id]['znak'] = '>'
             res['response']['text'] = 'Тогда начнем.    Задуманное число > %s ?' % (
                 sessiondiap[user_id]['itis'])
+            res['response']['buttons'] = get_yes_no(user_id)
+
         else:
+            res['response']['text'] = 'Хорошо.  Поиграем в другой раз...  Пока!'
             res['response']['end_session'] = True
         return
 
     # сюда попадаем когда начата игра
+    sp = ['Задуманное число', 'Загаданное число', 'Твоё число', 'Число']
     st = get_otvet(user_id, req['request']['original_utterance'].lower())
     if st == 'да' or st == 'нет':
         change_diap(user_id, st)
-#        res['response']['text'] = 'Все говорят "%s", а ты купи слона!' % (
-#            st)
-        res['response']['text'] = 'Твое число  > %s ?' % (
-                sessiondiap[user_id]['itis'])
+        if find_chislo(user_id):
+            res['response']['text'] = '%s, я уже  знаю твое число!  Было задумано число = %s.\
+            (мне потребовалось ходов = %s)' % (
+                sessionStorage[user_id]['first_name'].title(), sessiondiap[user_id]['tis'], sessiondiap[user_id]['step'])
+        else:
+            zn = random.randint(0, 1)
+            isp = random.randint(0, len(sp)-1)
+
+            put_name = random.randint(0, 1)
+            if put_name == 1:  #перед вопросом вывести имя
+                str0 = sessionStorage[user_id]['first_name'].title() + ', ' + sp[isp].lower()
+            else:
+                str0 = sp[isp]
+
+            if sessiondiap[user_id]['end'] - sessiondiap[user_id]['start'] == 1:
+                zn = 0
+                sessiondiap[user_id]['itis'] = sessiondiap[user_id]['end']
+                sessiondiap[user_id]['znak'] = '<'
+            if zn == 1:
+                res['response']['text'] = '%s > %s ?' % (
+                str0, sessiondiap[user_id]['itis']) #, sessiondiap[user_id]['start'], sessiondiap[user_id]['end'])
+ #               res['response']['text'] = '%s число  > %s?' % (
+ #               sp[isp], sessiondiap[user_id]['itis'])
+                sessiondiap[user_id]['znak'] = '>'
+            else:
+                res['response']['text'] = '%s  < %s  ?' % (
+                str0, sessiondiap[user_id]['itis']) #, sessiondiap[user_id]['start'], sessiondiap[user_id]['end'])
+#                res['response']['text'] = '%s число  < %s ?' % (
+#                sp[isp], sessiondiap[user_id]['itis'])
+                sessiondiap[user_id]['znak'] = '<'
+            res['response']['buttons'] = get_yes_no(user_id)
 #        req['request']['original_utterance'])
     else:
         res['response']['text'] = 'Не поняла твой ответ  "%s".  Пожалуйста, ответь еще раз!' % (
             req['request']['original_utterance'])
-    res['response']['buttons'] = get_suggests(user_id)
+#    res['response']['buttons'] = get_suggests(user_id)
 
 
 def get_otvet(user_id, st):
@@ -137,17 +173,40 @@ def change_diap(user_id, st):
         sessiondiap[user_id]['start'] = sessiondiap[user_id]['itis'] + 1
 
     elif sessiondiap[user_id]['znak'] == '>' and st == 'нет':
-        sessiondiap[user_id]['end'] = sessiondiap[user_id]['itis'] - 1
+        sessiondiap[user_id]['end'] = sessiondiap[user_id]['itis']
 
     elif sessiondiap[user_id]['znak'] == '<' and st == 'да':
         sessiondiap[user_id]['end'] = sessiondiap[user_id]['itis'] - 1
 
     elif sessiondiap[user_id]['znak'] == '<' and st == 'нет':
-            sessiondiap[user_id]['start'] = sessiondiap[user_id]['itis'] + 1
+            sessiondiap[user_id]['start'] = sessiondiap[user_id]['itis']
 
-    sessiondiap[user_id]['itis'] = int((sessiondiap[user_id]['end'] + sessiondiap[user_id]['start'] - 1) / 2)
+    sessiondiap[user_id]['itis'] = int((sessiondiap[user_id]['end'] + sessiondiap[user_id]['start']) / 2)
     sessiondiap[user_id]['step'] = sessiondiap[user_id]['step'] + 1
 
+
+def find_chislo(user_id):
+    if sessiondiap[user_id]['end'] - sessiondiap[user_id]['start'] <= 0:
+        if sessiondiap[user_id]['znak'] == '>':
+            sessiondiap[user_id]['tis'] = sessiondiap[user_id]['end']
+        else:  # sessiondiap[user_id]['znak'] == '<':
+            sessiondiap[user_id]['tis'] = sessiondiap[user_id]['start']
+        return True
+    else:
+        return False
+
+
+
+def get_yes_no(user_id):
+    session = sessionStorage[user_id]
+
+    # Выбираем две первые подсказки из массива.
+    mas = [
+        {'title': i, 'hide': True}
+        for i in session['yes_no']
+    ]
+
+    return mas
 
 # Функция возвращает две подсказки для ответа.
 def get_suggests(user_id):
